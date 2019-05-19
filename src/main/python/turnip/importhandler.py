6687c1e18ad2492388905a48035e5984c58ae87f
Pubudu Ranasinghe <pubudutr@gmail.com>
2019-05-19 19:23:56 +0530

    Add basic worker thread and importer.

--- src/main/python/turnip/importhandler.py
+++ src/main/python/turnip/importhandler.py
@@ -2,7 +2,10 @@ import os
 from beets.ui import UserError
 from beets import config, logging
 from turnipimporter import TurnipImportSession
-from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal, pyqtSlot, QUrl
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QUrl, QRunnable
+import traceback
+import sys
+import time
 
 
 def import_files(lib, paths, query):
@@ -32,10 +35,62 @@ def import_files(lib, paths, query):
 # ImportHandler class exposed to QML
 class ImportHandler(QObject):
 
-    def __init__(self, beets_facade):
+    def __init__(self, beets_facade, threadpool):
         QObject.__init__(self)
-        self.__beets = beets_facade
+        self._beets = beets_facade
+        self._threadpool = threadpool
+        print(f"Maximum of {self._threadpool.maxThreadCount()} threads.")
+
+    @pyqtSlot(QUrl)
+    def startSession(self, path):
+        session = self._beets.create_import_session(path.toLocalFile())
+        session.run()
+
+    @pyqtSlot(QUrl)
+    def test(self, path):
+        worker = Worker(self.some_work)
+        worker.signals.result.connect(self.print_output)
+        worker.signals.finished.connect(self.thread_complete)
+        self._threadpool.start(worker)
+
+    def print_output(self, s):
+        print(s)
+
+    def thread_complete(self):
+        print("THREAD COMPLETE!")
+
+    def some_work(self):
+        for n in range(0, 5):
+            time.sleep(1)
+            print(f"Working {n}")
+        return "Done"
+
+
+class Worker(QRunnable):
+    def __init__(self, fn, *args):
+        super(Worker, self).__init__()
+
+        self.fn = fn
+        self.args = args
+        self.signals = WorkerSignals()
 
     @pyqtSlot()
-    def start(self):
-        print("Starting")
+    def run(self):
+        try:
+            print("starting work")
+            result = self.fn(*self.args)
+        except Exception:  # TODO Stop catching every exception
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()
+
+
+class WorkerSignals(QObject):
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(int)
