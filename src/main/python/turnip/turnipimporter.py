096a4e5b51aabf455ec9f1edf2bc642e2e0a4856
Pubudu Ranasinghe <pubudutr@gmail.com>
2019-05-19 23:17:27 +0530

    Move things.

--- src/main/python/turnip/turnipimporter.py
+++ src/main/python/turnip/turnipimporter.py
@@ -1,15 +1,23 @@
-from beets import importer
+from beets.importer import ImportSession, action
+import logging
+import time
+
+logger = logging.getLogger("turnip")
+logger.setLevel(logging.DEBUG)
 
 
 # ImportSession class
-class TurnipImportSession(importer.ImportSession):
+class TurnipImportSession(ImportSession):
     """An import session that runs in GUI.
     """
+
     def choose_match(self, task):
-        print("{0} items".format(len(task.items)))
-        raise NotImplementedError
+        logger.info(f"{len(task.items)} items")
+        time.sleep(1)
+        return action.TRACKS
 
     def choose_item(self, task):
+        logger.info("choose an item")
         raise NotImplementedError
 
     def resolve_duplicate(self, task, found_duplicates):
